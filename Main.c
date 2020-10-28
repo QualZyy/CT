1d860fe89b6a3d7628c18bf954b2675c95487545
RolfOskarBgberg <62447104+RolfOskarBgberg@users.noreply.github.com>
2020-10-28 10:10:20 +0100

    Create Main.c
    Initial commit

---
+++ Main.c
@@ -0,0 +1,28 @@
+
+/*
+Bibliotek
+
+Struct person
+    Navn
+    Alder
+    Infisert ja/nei
+    Tidspunkt for smitte
+
+Struct rom
+    sted
+    String[][] besøk, hvem som var der, når de var der
+
+int main
+    Meny, hva vil en bruker gjøre
+
+    Registrer person
+    Slett person
+    Registrer rom
+    Slett rom
+    Registrer smitte på eksisterende person med tidspunkt
+    Registrer besøk - Hvis person går til et rom
+    Last opp fil - CSV?
+    Skriv til fil - CSV?
+
+
+*/
